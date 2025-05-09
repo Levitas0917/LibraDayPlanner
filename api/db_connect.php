a54c080454703a3e3b86a77f0acd59313c36474b
Levitas0917 <u3642175@connect.hku.hk>
2025-05-09 11:01:35 +0800

    Update db_connect.php

--- api/db_connect.php
+++ api/db_connect.php
@@ -1,18 +1,4 @@
 <?php
-$host = '127.0.0.1';
-$port = '11741'; 
-$db   = 'test1';
-$user = 'postgres';
-$pass = 'iAmTheBoss'; 
-
-
-$dsn = "pgsql:host=$host;port=$port;dbname=$db";
-$pdo = new PDO($dsn, $user, $pass);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
-
-
-<?php
 try {
     $host = '127.0.0.1';
     $port = '11741';
